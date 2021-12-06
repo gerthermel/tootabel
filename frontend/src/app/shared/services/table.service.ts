6617d073f5c2c57595f06096d78e17fe040e4470
gerth ermel <gerth.ermel@gmail.com>
2021-12-06 17:43:12 +0200

    Quick entry/edit

--- frontend/src/app/shared/services/table.service.ts
+++ frontend/src/app/shared/services/table.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, Input, Renderer2, RendererFactory2 } from '@angular/core';
+import { ElementRef, Injectable, Input, Renderer2, RendererFactory2 } from '@angular/core';
 import { FormControl, FormGroup, NgForm } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import { AlertService } from './alert.service';
 import { AppService } from './app.service';
 import { CompanyService } from './company.service';
+import * as _ from 'lodash' 
 
 @Injectable({
   providedIn: 'root'
@@ -36,6 +37,7 @@ export class TableService {
     10:'November',
     11:'Detsember',
   }
+
   public years = new Array();
   public weeksInMonth;
   @Input() selectedDay = this.currentDay;
@@ -47,25 +49,28 @@ export class TableService {
   public totalHours = 0;
   public tyymaad;
   public updateBuffer;
+  public entryStatus = {};
+  public entryQueue = {}; //queue of items that needs to be added, because first time is being inserted.
+  public saveHistory = {};
   private renderer:Renderer2;
   constructor(
     public service:AppService,
     public http: HttpClient,
     public alert:AlertService,
     public company:CompanyService,
-    public rendererFactory:RendererFactory2
+    public rendererFactory:RendererFactory2,
   ) { 
   }
   
-  getData(){
+  public getData(){
     this.http.get(environment.apiUrl+`/tootable/table/${this.service.selectedCompany}/hours/${this.filterSelectedMonth}/${this.filterSelectedYear}`, {withCredentials: true}).subscribe(
       (res)=>{
         this.hours = res['data'];
+        console.log(this.hours)
         this.isTableLoading = false;
         this.totalHours = res['hours'];
         this.tyymaad = res['tyymaad'];
         this.getWeeksInMonth();
-        console.log(this.weeksInMonth)
       },
       (error)=>{
 
@@ -73,9 +78,13 @@ export class TableService {
     )
   }
 
+  public filterObjects(){
+    console.log(this.tyymaad);
+  }
+
   public getDaysInMonth(month,year) {
     return new Date(year, month+1, 0).getDate();
-  };
+  }
 
   public getDays() {
     return new Date(this.selectedYear, this.filterSelectedMonth+1, 0).getDate();
@@ -139,9 +148,16 @@ export class TableService {
     this.company.closeSidebar()
   };
 
-  addHours(res){
-    let date = res['data']['date'];
-    this.hours[ date ][ res['data']['id'] ] = res['data'];
+  public addHours(res){
+    let data = res['data']
+    let date = data['date'];
+    console.log(data)
+    if(!this.hours['data'][ date ][ data['id'] ]){
+      this.hours['data'][ date ][ data['id'] ] = data
+    }else{
+      console.log(this.hours['data'][ date ][data['id']])
+      Object.assign(this.hours['data'][ date ][data['id']], data)
+    }
   }
 
   public generateCalendarArray(){
@@ -158,11 +174,11 @@ export class TableService {
     }
   }
 
-  getHours(){
+  public getHours(){
     return this.http.get(environment.apiUrl+`/tootable/table/${this.service.selectedCompany}/hours`, {withCredentials: true})
   }
 
-  prevMonth(){
+  public prevMonth(){
     this.isTableLoading = true;
     if( this.filterSelectedMonth > 0 ){
       this.filterSelectedMonth = this.filterSelectedMonth-1
@@ -170,7 +186,7 @@ export class TableService {
     }
   }
 
-  nextMonth(){
+  public nextMonth(){
     this.isTableLoading = true;
     if( this.filterSelectedMonth < 11 ){
       this.filterSelectedMonth = this.filterSelectedMonth+1
@@ -178,42 +194,180 @@ export class TableService {
     }
   }
 
-  quickNew(event){
-    var row = event.currentTarget.parentElement.parentElement
+  public checkRowSatus(date){
+    if( this.entryStatus[date] ){
+      return this.entryStatus[date]['loading']
+      /*
+      if( this.entryStatus[date] ){
+        var key = Object.keys(this.entryStatus[date])[0];
+        return this.entryStatus[date][key]['loading'];
+      }else{
+        return false;
+      }
+      */
+    }else{
+      return false;
+    }
+  }
+
+  public setRowStatus(date, status){
+    if( !this.entryStatus[date] ){
+      this.entryStatus[date] = {}
+    }
+    this.entryStatus[date] = {'loading':status,}
+  }
+
+  public insertIntoQueue(date, data){
+    
+    if( !this.entryQueue[date] ){
+      this.entryQueue[date] = {}
+    }
+    this.entryQueue[date][data.propertyName] = data;
+    console.log('We have inserted your data into queue')
+  }
+
+  public setSaveHistory(date, property, value){
+    if( !this.saveHistory[date] ){
+      this.saveHistory[date] = {}
+    }
+    if( !this.saveHistory[date][property] ){
+      this.saveHistory[date][property] = {}
+    }
+    this.saveHistory[date][property] = {'value': value}
+  }
+
+  public getSaveHistory(date, property, value){
+    if(this.saveHistory[date] !== undefined && this.saveHistory[date][property] !== undefined && this.saveHistory[date][property]['value'] == value){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+
+
+  public timeOut(event){
     clearTimeout(this.updateBuffer);
-    this.updateBuffer = setTimeout(()=>{
-      if( row.getAttribute('itemId')==0 ){
-        var value = event.target.value;
-        var propertyName = event.target.getAttribute('property')
-        var formData: FormData = new FormData();
-        formData.append('company_id', this.service.selectedCompany);
-        formData.append('date', `${this.selectedYear}-${this.selectedMonth+1}-${row.getAttribute('day')}`);
-        formData.append(`${propertyName}`, value);
-        this.insertNew(formData).subscribe(
-          (res)=>{
-            this.addHours(res); 
-            var id = res['data']['id'];
-            row.setAttribute('itemId',id)
-          },
-          (error)=>{
-            this.alert.error(error.error.message)
-          }
-        );
+    this.updateBuffer = setTimeout(()=>{ 
+      this.quickNew(event)
+    },800)
+
+  }
+
+  public focusOut(event){
+    var row = event.target.parentElement.parentElement
+    var date = row.getAttribute('date')
+    var propertyName = event.target.getAttribute('property')
+    var value = event.target.innerText;
+    clearTimeout(this.updateBuffer);
+    if( !this.getSaveHistory(date, propertyName, value)){
+      this.quickNew(event)
+    }
+  }
+
+  public async getRowInputs(event){
+    console.log('getting input of all td')
+    return new Promise(resolve => {
+      let data = {}
+      var inputList = event.target.parentElement.parentNode.children;
+      for (let key in inputList) {
+        var propertyName = inputList[key].getAttribute('property')
+        if(propertyName){
+          console.log('returning all input of all td')
+          var value = inputList[key].innerText
+          
+          data[propertyName] = value
+          console.log(data);
+        }
+      }
+      resolve(data);
+    })
+  }
+
+  public async  quickNew(event){
+    console.log('quickNew')
+    var rowInputs = this.getRowInputs(event)
+      var row = event.target.parentElement.parentElement
+      var date = row.getAttribute('date')
+      var propertyName = event.target.getAttribute('property')
+      var value = event.target.innerText;
+
+      //We save so that later we can check if we need to run insertion again or not
+      this.setSaveHistory(date, propertyName, value)
+
+      var formData: FormData = new FormData();
+      formData.append('company_id', this.service.selectedCompany);
+      formData.append('date', date);
+      formData.append(`${propertyName}`, value);
+
+      if( row.getAttribute('itemId')==0 ){ //if row has been inserted once before then there is id present
+        
+        if( this.checkRowSatus(date) ){
+          //we need to edit when first insert of row is done and id has been set so we add these into queue
+          console.log('we have already insertion in progress so we add into queue instead')
+          this.insertIntoQueue(date, {'company_id':this.service.selectedCompany, 'date':date,'propertyName':propertyName,'value':value,'loading':true,})
+        }else{
+          this.setRowStatus(date, true) //we set row into loading so that we can check it
+          this.insertNew(formData).subscribe(
+            (res)=>{
+              var id = res['data']['id'];
+               //we get other td values and merge them
+              console.log('-----------------')
+              Object.assign(this.hours['data'][date][0], res['data']); 
+              delete this.hours['data'][date][0]
+              console.log('-----------------')
+              this.addHours(res); //once they merged we insert into main object variable
+              row.setAttribute('itemId',id)
+              this.setRowStatus(date, false)
+              console.log('Lets check if anything is in queue')
+              if( this.entryQueue[date] ){
+                console.log('There are items in que')
+                for (let key in this.entryQueue[date]) {
+                  let item = this.entryQueue[date][key];
+                  let object = {
+                    'id':id,
+                    'property':item.propertyName,
+                    'value':item.value
+                  }
+                  Object.assign(this.hours['data'][date][id], {[item.propertyName]:item.value}); 
+                  this.editQuick(object);
+                  this.entryQueue[date].splice(key, 1);
+                }
+              }
+
+            },
+            (error)=>{
+              this.alert.error(error.error.message)
+            }
+          );
+        }
+
       }else{
         console.log('needs to edit')
       }
-    },800)
   }
 
   insertNew(form){
     return this.http.post(environment.apiUrl+'/tootable/table/new-entry', form, {withCredentials: true})
   }
-  
+
+  editQuick(object){
+    var formData: FormData = new FormData();
+    formData.append('id', object.id);
+    formData.append('property', object.property);
+    formData.append('value', object.value);
+    this.http.put(environment.apiUrl+'/tootable/table/edit-entry', formData, {withCredentials: true}).subscribe(
+        (res)=>{
+          //this.addHours(res); 
+        }
+      )
+  }
+
   edit(event){
     var itemId = event.currentTarget.parentElement.parentElement.getAttribute('itemId');
     clearTimeout(this.updateBuffer);
     this.updateBuffer = setTimeout(()=>{
-      var value = event.target.value;
+      var value = event.target.innerText;
       var propertyName = event.target.getAttribute('property')
       var formData: FormData = new FormData();
       formData.append('id', itemId);
@@ -228,7 +382,7 @@ export class TableService {
 
   }
 
- getWeeksInMonth() {
+  getWeeksInMonth() {
     const year = this.selectedYear
     const month = this.selectedMonth
     const weeks = [],
@@ -258,13 +412,14 @@ export class TableService {
     var itemId =  id;
     this.http.delete(environment.apiUrl+`/tootable/table/entry/${id}`, {withCredentials: true}).subscribe(
       (res)=>{
-        delete this.hours[date][id];
+        delete this.hours['data'][date][id];
       },
       (error)=>{
         this.alert.error(error.error.message)
       }
     )
   }
+
   convertToDate(string){
     console.log(string)
     return  new Date(string)
